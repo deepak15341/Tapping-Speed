2486f86933cc2167758cf7ec6674f9b38a95c3ac
deepak Kashyap <deepakkumarsahbgp123@gmail.com>
2023-03-20 12:09:16 +0530

    1.0

--- app/src/main/java/com/deepak15341/tappingspeed/MainActivity.java
+++ app/src/main/java/com/deepak15341/tappingspeed/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         TextView timer = findViewById(R.id.timer);
         ImageView image = findViewById(R.id.imageView);
@@ -34,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
                     cd=cd-1;
                     CountDown.setText(cd+"");
                 }
+
+                CountDownTimer countDownTimer= new CountDownTimer(60000,1000) {
+                    @Override
+                    public void onTick(long l) {
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+
+                    }
+                };
             }
         });
     }
