60019fb87eb8508b6abd23af7c178096fd387320
deeoak15341 <deepakkumarsahbgp123@gmail.com>
2023-03-22 17:58:20 +0530

    2.0

--- app/src/main/java/com/deepak15341/tappingspeed/MainActivity.java
+++ app/src/main/java/com/deepak15341/tappingspeed/MainActivity.java
@@ -1,9 +1,11 @@
 package com.deepak15341.tappingspeed;
 
 
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -11,6 +13,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CountDownTimer countDownTimer;
+    private long totaltime =5000;
+    private int remaining = 0,timeinterval =1000;
+    int count=0;
+    int cu=0,cd=1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +32,40 @@ public class MainActivity extends AppCompatActivity {
         TextView CountUp = findViewById(R.id.countup);
         TextView CountDown = findViewById(R.id.CountDown);
         AppCompatButton appCompatButton =findViewById(R.id.appCompatButton);
+        AppCompatButton appCompatButtonreset = findViewById(R.id.appCompatButtonReset);
+        image.setImageDrawable(getDrawable(R.drawable.startimg));
+
         appCompatButton.setOnClickListener(new View.OnClickListener() {
-            int cu=0,cd=1000;
             @Override
             public void onClick(View view) {
+                appCompatButtonreset.setClickable(false);
+                appCompatButton.setBackgroundColor(getResources().getColor(R.color.orange));
 
+                if (count==0){
+                    count=count+1;
+                    CountDownTimer countDownTimer= new CountDownTimer(totaltime,timeinterval) {
+                        @Override
+                        public void onTick(long l) {
+                            remaining = (int)l/1000;
+                            timer.setText(remaining +"");
+
+
+                        }
+
+                        @Override
+                        public void onFinish(){
+                            timer.setText(0+"");
+                            image.setImageDrawable(getDrawable(R.drawable.game_over));
+                            appCompatButton.setClickable(false);
+                            appCompatButton.setBackgroundColor(getResources().getColor(R.color.grey));
+                            appCompatButtonreset.setClickable(true);
+                            appCompatButtonreset.setBackgroundColor(getResources().getColor(R.color.orange));
+
+                        }
+                    };
+                    countDownTimer.start();
+
+                }
                 if (cu<=1000) {
                     cu=cu+1;
                     CountUp.setText(cu+"");
@@ -38,18 +75,27 @@ public class MainActivity extends AppCompatActivity {
                     CountDown.setText(cd+"");
                 }
 
-                CountDownTimer countDownTimer= new CountDownTimer(60000,1000) {
-                    @Override
-                    public void onTick(long l) {
 
-                    }
-
-                    @Override
-                    public void onFinish() {
-
-                    }
-                };
             }
         });
+        appCompatButtonreset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.setText(0+"");
+                image.setImageDrawable(getDrawable(R.drawable.startimg));
+                count=0;
+                cu=0;
+                cd=1000;
+                appCompatButton.setClickable(true);
+                appCompatButtonreset.setBackgroundColor(getResources().getColor(R.color.grey));
+                CountDown.setText(0+"");
+                CountUp.setText(0+"");
+
+            }
+        });
+
+
+
+
     }
 }
