a5e3096c8c29ff9a814a0c000d5c01fe7c593264
deeoak15341 <deepakkumarsahbgp123@gmail.com>
2023-03-23 16:45:05 +0530

    4.0

--- app/src/main/java/com/deepak15341/tappingspeed/MainActivity.java
+++ app/src/main/java/com/deepak15341/tappingspeed/MainActivity.java
@@ -2,6 +2,7 @@ package com.deepak15341.tappingspeed;
 
 
 import androidx.annotation.ColorRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private int remaining = 0,timeinterval =1000;
     int count=0;
     int cu=0,cd=400,fcd=400;
+    AppCompatButton appCompatButtonreset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,49 +36,20 @@ public class MainActivity extends AppCompatActivity {
         TextView CountUp = findViewById(R.id.countup);
         TextView CountDown = findViewById(R.id.CountDown);
         AppCompatButton appCompatButton =findViewById(R.id.appCompatButton);
-        AppCompatButton appCompatButtonreset = findViewById(R.id.appCompatButtonReset);
+        appCompatButtonreset = findViewById(R.id.appCompatButtonReset);
         image.setImageDrawable(getDrawable(R.drawable.startimg));
+        CountDown.setText(cd+"");
+        CountUp.setText(cu+"");
 
         appCompatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appCompatButtonreset.setClickable(false);
+
                 appCompatButton.setBackgroundColor(getResources().getColor(R.color.orange));
 
                 if (count==0){
                     count=count+1;
-                    CountDownTimer countDownTimer= new CountDownTimer(totaltime,timeinterval) {
-                        @Override
 
-                        public void onTick(long l) {
-                            remaining = (int)l/1000;
-                            timer.setText(remaining +"");
-                            result.setText("Go On,You can achieve!");
-
-
-
-                        }
-
-                        @Override
-                        public void onFinish(){
-                            if(cu==fcd) {
-                                image.setImageDrawable(getDrawable(R.drawable.you_won));
-                                result.setText("Great");
-                                timer.setText(0 + "");
-                            }
-                            else {
-
-                                timer.setText(0 + "");
-                                result.setText("Better Luck Next Time");
-                                image.setImageDrawable(getDrawable(R.drawable.game_over));
-                                appCompatButton.setClickable(false);
-                                appCompatButton.setBackgroundColor(getResources().getColor(R.color.grey));
-                                appCompatButtonreset.setClickable(true);
-                                appCompatButtonreset.setBackgroundColor(getResources().getColor(R.color.orange));
-                            }
-
-                        }
-                    };
                     countDownTimer.start();
 
                 }
@@ -84,39 +58,71 @@ public class MainActivity extends AppCompatActivity {
                     result.setText("Great");
                     appCompatButton.setClickable(false);
                     appCompatButton.setBackgroundColor(getResources().getColor(R.color.grey));
-                    appCompatButtonreset.setClickable(true);
-                    appCompatButtonreset.setBackgroundColor(getResources().getColor(R.color.orange));
                 }
                 else {
-                    if (cu <= fcd) {
-                        cu = cu + 1;
+                    if (cu < fcd) {
+                        cu++;
                         CountUp.setText(cu + "");
                     }
-                    if (cd >= 0) {
-                        cd = cd - 1;
+                    if (cd > 0) {
+                        cd--;
                         CountDown.setText(cd + "");
                     }
                 }
             }
         });
+
+
+
         appCompatButtonreset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                countDownTimer.cancel();
                 timer.setText(0+"");
                 image.setImageDrawable(getDrawable(R.drawable.startimg));
                 count=0;
                 cu=0;
                 cd=fcd;
                 appCompatButton.setClickable(true);
-                appCompatButtonreset.setBackgroundColor(getResources().getColor(R.color.grey));
-                CountDown.setText(0+"");
-                CountUp.setText(0+"");
+                CountDown.setText(cd+"");
+                CountUp.setText(cu+"");
+                countDownTimer.cancel();
+
 
             }
         });
 
+        countDownTimer= new CountDownTimer(totaltime,timeinterval) {
+            @Override
+
+            public void onTick(long l) {
+                remaining = (int)l/1000;
+                timer.setText(remaining +"");
+                result.setText("Go On,You can achieve!");
 
 
+            }
+
+            @Override
+            public void onFinish(){
+                if(cu==fcd) {
+                    image.setImageDrawable(getDrawable(R.drawable.you_won));
+                    result.setText("Great");
+                    timer.setText(0 + "");
+                    appCompatButton.setClickable(false);
+                    appCompatButton.setBackgroundColor(getResources().getColor(R.color.grey));
+                }
+                else {
+
+                    timer.setText(0 + "");
+                    result.setText("Better Luck Next Time");
+                    image.setImageDrawable(getDrawable(R.drawable.game_over));
+                    appCompatButton.setClickable(false);
+                    appCompatButton.setBackgroundColor(getResources().getColor(R.color.grey));
+                }
+
+            }
+        };
 
     }
 }
